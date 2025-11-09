bef96f2e39b3c7f19ff966c6aa6a7b40593c10a5
Hoai Nguyen <hoainb1303@gmail.com>
2025-11-09 23:48:15 +0700

    Add hero section

---
+++ app/components/Hero.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Hero = () => {
+  return <div>Hi my name is Hoai</div>;
+};
+
+export default Hero;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <main>
       <Header />
-      <div className="pt-20 h-1000 bg-blue-200">hello world</div>
+      <Hero />
     </main>
   );
 }
