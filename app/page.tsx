be940c0d653d012fd1984fc9de049cf53790e276
Hoai Nguyen <hoainb1303@gmail.com>
2025-11-10 18:38:58 +0700

    Practing NextJS routing system by adding a mocking contact page

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,8 @@ import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <main>
-      <Header />
-      <Hero />
-    </main>
+    <>
+      <Hero></Hero>
+    </>
   );
 }
