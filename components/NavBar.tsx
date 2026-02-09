53c28feffb2adad3010ffab2c0f54e4462b2b8f8
Hoai Nguyen <hoainb1303@gmail.com>
2026-02-09 23:56:31 +0700

    A fresh beginning

---
+++ components/NavBar.tsx
@@ -0,0 +1,48 @@
+"use client";
+import React from "react";
+import { useState } from "react";
+import { Link as ScrollLink } from "react-scroll";
+
+import styles from "./NavBar.module.css";
+
+const NavBar = () => {
+  return (
+    <nav className={`${styles.navBar}`}>
+      <div className={styles.container}>
+        <div className={styles.navHeader}>
+          <div className={styles.navLogo}></div>
+          <div className={styles.navHam}></div>
+        </div>
+        <ul className={styles.navLinks}>
+          <li className={styles.navItem}>
+            <ScrollLink to="hero" smooth="true">
+              HOME
+            </ScrollLink>
+          </li>
+          <li className={styles.navItem}>
+            <ScrollLink to="skills" smooth="true">
+              SKILLS
+            </ScrollLink>
+          </li>
+          <li className={styles.navItem}>
+            <ScrollLink to="projects" smooth="true">
+              PROJECTS
+            </ScrollLink>
+          </li>
+          <li className={styles.navItem}>
+            <ScrollLink to="about" smooth="true">
+              ABOUT
+            </ScrollLink>
+          </li>
+          <li className={styles.navItem}>
+            <ScrollLink to="contact" smooth="true">
+              CONTACT
+            </ScrollLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
